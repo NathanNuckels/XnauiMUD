e94861f663864ea54c9100a7bd239c83dccabd6f
Nathan Nuckels <nathannuckels@gmail.com>
2022-05-16 11:38:23 -0500

    Inital commit

---
+++ src/main/java/com/phoenix/mud/App.java
@@ -0,0 +1,13 @@
+package com.phoenix.mud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
