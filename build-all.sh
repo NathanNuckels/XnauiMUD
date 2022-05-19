83306211d8a7dee961c7cce87b4438cefc145885
Nathan Nuckels <nathannuckels@gmail.com>
2022-05-19 09:45:43 -0500

    Placeholders

---
+++ build-all.sh
@@ -0,0 +1,1 @@
+mvn -B clean verify site javadoc:javadoc
--- src/main/java/com/phoenix/mud/App.java
+++ src/main/java/com/phoenix/mud/App.java
@@ -1,13 +1,22 @@
 package com.phoenix.mud;
 
 /**
- * Hello world!
+ * Main class to run the demo.
  *
+ * Running this class runs the demo
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	/**
+	 * Runs the demo.
+	 *
+	 * This is the demo of the MUD system.
+	 * It uses all of the classes in com.phoenix.mud.demo
+	 * @param args Unused
+	 * @return None
+	 */
+	public static void main(String[] args){
+		System.out.println("Welcome to XnauiMUD!\nAll classes used are in com.phoenix.mud.demo");
+		System.out.println("Open the maven site (target/site/index.html), \nclick reporting, then open the javadoc for more info");
+	}
 }
