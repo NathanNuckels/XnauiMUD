516f45aa09afbf1b65c5e1ca9b5fd2345a0465fd
Nathan Nuckels <nathannuckels@gmail.com>
2022-05-19 09:45:30 -0500

    Added javadoc thingy

---
+++ src/main/java/com/phoenix/mud/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the core of the MUD.
+ *
+ * This is the core of the MUD. It processes commands, saving and loading, and keeps all the data together
+ * The sub-packages contain base classes and all varients of them. the demo package has examples of
+ * useing the API
+ */
+
+package com.phoenix.mud;
